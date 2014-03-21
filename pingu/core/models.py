8d444d5d0f9e3c82f87ca51a775577b2f941f018
agconti <conti@fueled.com>
2014-03-21 13:02:27 -0400

    feat(core): added rankings model

--- pingu/core/models.py
+++ pingu/core/models.py
@@ -10,5 +10,24 @@ class Match(TimeStampedModel):
 
 class Score(TimeStampedModel):
     match = models.ForeignKey(Match, related_name="match")
-    player = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="player")
+    player = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="player+")
     score = models.IntegerField()
+
+
+class Ranking(TimeStampedModel):
+    player = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="player")
+    best_score = models.IntegerField()
+    worst_score = models.IntegerField()
+    heighest_ranking = models.IntegerField()
+
+    @property
+    def calculate_elo_ranking(self):
+        pass
+
+    @property
+    def calculate_best_score(self):
+        pass
+
+    @property
+    def calculate_worst_score(self):
+        pass
