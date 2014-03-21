d1d65f36d92de81731596aa81a2fa97d5e09fccb
agconti <conti@fueled.com>
2014-03-21 12:51:20 -0400

    feat(core): added score model

--- pingu/core/models.py
+++ pingu/core/models.py
@@ -1,11 +1,14 @@
 from model_utils.models import TimeStampedModel
 from django.db import models
-from users.models import User
 from config import settings
-
-# Create your models here.
 
 
 class Match(TimeStampedModel):
     winner = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="winner")
     loser = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="loser")
+
+
+class Score(TimeStampedModel):
+    match = models.ForeignKey(Match, related_name="match")
+    player = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="player")
+    score = models.IntegerField()
