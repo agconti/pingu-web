f460c1b2488f5876107fb29c3dce6f191128ebd6
awc-fueled <conti@fueled.com>
2014-03-24 14:46:52 -0400

    fix(api): updated user serialier to only use necessary fields

--- pingu/api/serializers.py
+++ pingu/api/serializers.py
@@ -9,12 +9,6 @@ class MatchSerializer(serializers.HyperlinkedModelSerializer):
         model = Match
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-
-    class Meta:
-        model = User
-
-
 class ScoreSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
@@ -25,3 +19,14 @@ class RankingSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = Ranking
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    match = MatchSerializer(many=True, read_only=True)
+    ranking = RankingSerializer(many=True, read_only=True)
+    auth_token = serializers.SlugRelatedField(read_only=True, slug_field='key')
+
+    class Meta:
+        model = User
+        fields = ('url', 'first_name', 'last_name',
+                  'username', 'auth_token')
