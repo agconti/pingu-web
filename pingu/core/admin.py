c8e20a4e482addc5768aec1026824237aeb24b67
awc-fueled <conti@fueled.com>
2014-03-26 14:28:58 -0400

    feat(core): added models to admin

--- pingu/core/admin.py
+++ pingu/core/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Match, Score, Ranking
 
-# Register your models here.
+
+class ScoreAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Score
+
+
+class MatchAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Match
+
+
+class RankingAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Ranking
+
+
+admin.site.register(Score, ScoreAdmin)
+admin.site.register(Match, MatchAdmin)
+admin.site.register(Ranking, RankingAdmin)
