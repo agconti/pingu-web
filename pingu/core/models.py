2eb0f9714a6a6b3aeba81ea41bac7cfa67ce8de8
awc-fueled <conti@fueled.com>
2014-05-27 16:06:46 -0400

    feat(core): added stats on save for ranking model

--- pingu/core/models.py
+++ pingu/core/models.py
@@ -18,22 +18,26 @@ class Score(TimeStampedModel):
 class Ranking(TimeStampedModel):
     player = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="rank")
     elo_rating = models.IntegerField(default=1400)
-    best_score = models.IntegerField(default=0)
-    worst_score = models.IntegerField(default=0)
+    best_score_differential = models.FloatField(default=0)
+    worst_score_differential = models.IntegerField(default=0)
     heighest_ranking = models.IntegerField(default=0)
 
-
     def save(self, match, *args, **kwargs):
-        score = match.score.match_score
-        self.elo_rating = self.calculate_elo_rating(match)
-        self.best_score = self.calculate_best_score(score)
-        self.worst_score = self.calculate_worst_score(score)
-        self.heighest_ranking = self.calculate_heighest_ranking(score)
+        '''
+        Updates a user's ranking with a match.
+        '''
+        self.calculate_elo_rating(match)
+        if match.winner == self.player:
+            self.calculate_best_score(match)
+        else:
+            self.calculate_worst_score(match)
+        self.calculate_heighest_ranking()
         super(Ranking, self).save(*args, **kwargs)
 
-
-    @classmethod
     def calculate_elo_rating(self, match):
+        '''
+        Caclulates the elo score for a user
+        '''
         win_value = 1
         loss_value = 0
         kfactor = 32
@@ -41,23 +45,29 @@ class Ranking(TimeStampedModel):
             rank_difference = self.elo_rating - match.loser.rank.elo_rating
         else:
             rank_difference = self.elo_rating - match.winner.rank.elo_rating
-        win_chance = 1 / ( 1 + ( math.pow(10,( rank_difference / 400 ))))
-        self.elo_rating = self.elo_rating + kfactor*( win_value - win_chance )
+        win_chance = 1 / (1 + (math.pow(10, (rank_difference / 400))))
+        self.elo_rating = self.elo_rating + kfactor * (win_value - win_chance)
 
+    def calculate_best_score_differential(self, match):
+        """
+        Needs to be refactored to differential
+        """
+        winner_score = match.score.winner
+        looser_score = match.score.loser
+        differential = (winner_score - looser_score) / winner_score
+        if self.best_score > differential:
+            self.best_score = differential
 
-    @classmethod
-    def calculate_best_score(self, score):
-        if self.best_score > score:
-            self.best_score = score
+    def calculate_worst_score_differential(self, match):
+        """
+        Needs to be refactored to differential
+        """
+        winner_score = match.score.winner
+        looser_score = match.score.loser
+        differential = (winner_score - looser_score) / winner_score
+        if self.worst_score < differential:
+            self.worst_score = differential
 
-
-    @classmethod
-    def calculate_worst_score(self, score):
-        if self.worst < score:
-            self.worst = score
-
-
-    @classmethod
     def calculate_heighest_ranking(self):
         if self.heighest_ranking < self.elo_rating:
             self.heighest_ranking = self.elo_rating
